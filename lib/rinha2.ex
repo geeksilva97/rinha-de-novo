df9ba9bb112dedf01e7b695dc8933d1d00ba57b5
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-16 18:39:58 -0300

    nothing interesting

---
+++ lib/rinha2.ex
@@ -0,0 +1,18 @@
+defmodule Rinha2 do
+  @moduledoc """
+  Documentation for `Rinha2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rinha2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rinha2_test.exs
@@ -0,0 +1,8 @@
+defmodule Rinha2Test do
+  use ExUnit.Case
+  doctest Rinha2
+
+  test "greets the world" do
+    assert Rinha2.hello() == :world
+  end
+end
