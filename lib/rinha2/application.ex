19788d831839fda5588da497419f9a215fe74abb
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-16 22:35:28 -0300

    cluster starting

--- lib/rinha2/application.ex
+++ lib/rinha2/application.ex
@@ -8,8 +8,16 @@ defmodule Rinha2.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Rinha2.Worker.start_link(arg)
-      # {Rinha2.Worker, arg}
+      {Task, fn ->
+        Logger.info("#{node()} attempting to connect to #{bootstrap_node()}")
+
+        case Node.connect(bootstrap_node()) do
+          true ->
+            Logger.info("Connection succeeded. We are a cluster")
+          _ ->
+            Logger.info("Could not connect to #{bootstrap_node()}")
+        end
+      end}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -20,5 +28,9 @@ defmodule Rinha2.Application do
     Logger.info("Starting application at node #{node()}")
 
     Supervisor.start_link(children, opts)
+  end
+
+  def bootstrap_node() do
+    System.get_env("BOOTSTRAP_NODE", "fook") |> String.to_atom()
   end
 end
