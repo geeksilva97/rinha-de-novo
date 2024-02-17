c4b1c6dc65b1db66960579750b7efb646f34bb52
geeksilva97 <edigleyssonsilva@gmail.com>
2024-02-16 23:18:56 -0300

    base implementation for summary handler

--- lib/rinha2/interface/summary_handler.ex
+++ lib/rinha2/interface/summary_handler.ex
@@ -1,9 +1,22 @@
 defmodule Rinha2.Interface.SummaryHandler do
+  require Logger
+
   def init(req, options) do
-    req = :cowboy_req.reply(200, %{
-      <<"content-type">> => <<"text/plain">>
-    }, <<"Rota de extrato">>, req)
+    method = :cowboy_req.method(req)
+
+    req = handle_req(method, req)
 
     {:ok, req, options}
+  end
+
+  def handle_req(<<"GET">>, req) do
+    client_id = :cowboy_req.binding(:client_id, req, <<"0">>) |> :erlang.binary_to_integer()
+
+    case client_id > 0 and client_id < 6 do
+      true ->
+        :cowboy_req.reply(204, req)
+      false ->
+        :cowboy_req.reply(404, req)
+    end
   end
 end
